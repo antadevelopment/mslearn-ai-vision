7e6010e87362afe233a38b0b018a27b6b0205846
anta <braiandebarros@hotmail.com>
2024-09-21 19:21:45 -0300

    analyze-face almost done

--- Labfiles/04-face/C-Sharp/face-api/Program.cs
+++ Labfiles/04-face/C-Sharp/face-api/Program.cs
@@ -6,15 +6,15 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
-// Import namespaces
-
+using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
 
 
 namespace analyze_faces
 {
     class Program
     {
-
+        //https://learn.microsoft.com/en-us/azure/ai-services/computer-vision/overview-identity
         private static FaceClient faceClient;
         static async Task Main(string[] args)
         {
@@ -27,7 +27,11 @@ namespace analyze_faces
                 string cogSvcKey = configuration["AIServiceKey"];
 
                 // Authenticate Face client
-
+                ApiKeyServiceClientCredentials credentials = new ApiKeyServiceClientCredentials(cogSvcKey);
+                faceClient = new FaceClient(credentials)
+                {
+                    Endpoint = cogSvcEndpoint
+                };
 
                 // Menu for face functions
                 Console.WriteLine("1: Detect faces\nAny other key to quit");
@@ -53,11 +57,65 @@ namespace analyze_faces
             Console.WriteLine($"Detecting faces in {imageFile}");
 
             // Specify facial features to be retrieved
-
+            IList<FaceAttributeType> features = new FaceAttributeType[]
+            {
+                 FaceAttributeType.Occlusion,
+                 FaceAttributeType.Blur,
+                 FaceAttributeType.Glasses,
+                 FaceAttributeType.Age,
+                 FaceAttributeType.Hair,
+                 FaceAttributeType.Gender
+            };
 
             // Get faces
- 
- 
+            using (var imageData = File.OpenRead(imageFile))
+            {
+                var detected_faces = await faceClient.Face.DetectWithStreamAsync(imageData, returnFaceAttributes: features, returnFaceId: false);
+
+                if (detected_faces.Count() > 0)
+                {
+                    Console.WriteLine($"{detected_faces.Count()} faces detected.");
+
+                    // Prepare image for drawing
+                    Image image = Image.FromFile(imageFile);
+                    Graphics graphics = Graphics.FromImage(image);
+                    Pen pen = new Pen(Color.LightGreen, 3);
+                    Font font = new Font("Arial", 4);
+                    SolidBrush brush = new SolidBrush(Color.White);
+                    int faceCount = 0;
+
+                    // Draw and annotate each face
+                    foreach (var face in detected_faces)
+                    {
+                        faceCount++;
+                        Console.WriteLine($"\nFace number {faceCount}");
+
+                        // Get face properties
+                        Console.WriteLine($" - Mouth Occluded: {face.FaceAttributes.Occlusion.MouthOccluded}");
+                        Console.WriteLine($" - Eye Occluded: {face.FaceAttributes.Occlusion.EyeOccluded}");
+                        Console.WriteLine($" - Blur: {face.FaceAttributes.Blur.BlurLevel}");
+                        Console.WriteLine($" - Glasses: {face.FaceAttributes.Glasses}");
+                        Console.WriteLine($" - Age: {face.FaceAttributes.Age}");
+                        Console.WriteLine($" - Hair: {face.FaceAttributes.Hair}");
+                        Console.WriteLine($" - Gender: {face.FaceAttributes.Gender}");
+
+
+
+                        // Draw and annotate face
+                        var r = face.FaceRectangle;
+                        Rectangle rect = new Rectangle(r.Left, r.Top, r.Width, r.Height);
+                        graphics.DrawRectangle(pen, rect);
+                        string annotation = $"Face number {faceCount}";
+                        graphics.DrawString(annotation, font, brush, r.Left, r.Top);
+                    }
+
+                    // Save annotated image
+                    String output_file = "detected_faces.jpg";
+                    image.Save(output_file);
+                    Console.WriteLine(" Results saved in " + output_file);
+                }
+            }
+
         }
     }
 }
